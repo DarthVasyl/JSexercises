ae4ae2432685ef9dd703e843d80d2f086351ee11
darth_vasyl <formicking@gmail.com>
2017-06-02 12:11:35 +0300

    Reverse a String

---
+++ Basic Algorithm Scripting/Reverse a String.js
@@ -0,0 +1,17 @@
+/** DESCRIPTION */
+/*
+	Reverse the provided string.
+	You may need to turn the string into an array before you can reverse it.
+	Your result must be a string.
+*/
+
+/** Basic solution*/
+
+function reverseString(str) {
+	return str.split('').reverse().join('');
+}
+
+/** Test calls */
+reverseString("hello"); // should become "olleh".
+reverseString("Howdy"); // should become "ydwoH".
+reverseString("Greetings from Earth"); // should return "htraE morf sgniteerG"
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # JSexercises
+This repo created for my training exercices from freeCodeCamp.
+I'll try to solve problems at least in two different ways.
+
+Also it will be written in this format: 
+- task desriptions
+- solving
+- some test calls.
