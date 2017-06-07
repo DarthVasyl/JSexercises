b0deb691eae9ec11ee176c0ecdf0b110ff693e48
darth_vasyl <formicking@gmail.com>
2017-06-07 17:44:08 +0300

    Boo who

---
+++ Intermediate Algorithm Scripting/Boo who.js
@@ -0,0 +1,26 @@
+/** DESCRIPTION */
+/*
+Check if a value is classified as a boolean primitive. Return true or false.
+Boolean primitives are true and false.
+*/
+
+/** Simple  solution */
+function booWho(bool) {
+  if(bool === true || bool === false) return true;
+  return false;
+}
+/** Simple solution */
+function booWho(bool) {
+return (typeof bool === 'boolean') ? true : false;
+}
+/** Test calls */
+booWho(true);// should return true.
+booWho(false);// should return true.
+booWho([1, 2, 3]);// should return false.
+booWho([].slice);// should return false.
+booWho({ "a": 1 });// should return false.
+booWho(1);// should return false.
+booWho(NaN);// should return false.
+booWho("a");// should return false.
+booWho("true");// should return false.
+booWho("false");// should return false.
