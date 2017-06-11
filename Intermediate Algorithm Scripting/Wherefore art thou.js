3fe85971a208e3cbae5e6c05384c8364520665fe
darth_vasyl <formicking@gmail.com>
2017-06-11 13:48:50 +0300

    Wherefore art thou

---
+++ Intermediate Algorithm Scripting/Wherefore art thou.js
@@ -0,0 +1,91 @@
+/** DESCRIPTION */
+/*
+Make a function that looks through an array of objects (first argument) and returns an array of all objects that have matching property and value pairs (second argument). Each property and value pair of the source object has to be present in the object from the collection if it is to be included in the returned array.
+For example, if the first argument is [{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], and the second argument is { last: "Capulet" }, then you must return the third object from the array (the first argument), because it contains the property and its value, that was passed on as the second argument.
+*/
+
+/** Nested loops solution */
+function whatIsInAName(collection, source) {
+  // What's in a name?
+  var arr = [];
+  // Only change code below this line
+  for (var i = 0; i < collection.length; i++) {
+    var coincidence = true;
+    for (var prop in source) {
+      if (!(collection[i].hasOwnProperty(prop) && source[prop] === collection[i][prop])) {
+        coincidence = false;
+      }
+    }
+    if (coincidence) {
+      arr.push(collection[i]);
+    }
+  }
+  // Only change code above this line
+  return arr;
+
+  function whatIsInAName(collection, source) {
+    // What's in a name?
+    var arr = [];
+    // Only change code below this line
+    arr = collection.filter(function(el) {
+      for (var prop in source) {
+        if (!el.hasOwnProperty(prop) || source[prop] !== el[prop]) {
+          return false;
+        }
+      }
+      return true;
+    });
+    // Only change code above this line
+    return arr;
+  }
+  /** Simple solution */
+
+  /** Test calls */
+  whatIsInAName([{
+    first: "Romeo",
+    last: "Montague"
+  }, {
+    first: "Mercutio",
+    last: null
+  }, {
+    first: "Tybalt",
+    last: "Capulet"
+  }], {
+    last: "Capulet"
+  }); // should return [{ first: "Tybalt", last: "Capulet" }].
+  whatIsInAName([{
+    "a": 1
+  }, {
+    "a": 1
+  }, {
+    "a": 1,
+    "b": 2
+  }], {
+    "a": 1
+  }); // should return [{ "a": 1 }, { "a": 1 }, { "a": 1, "b": 2 }].
+  whatIsInAName([{
+    "a": 1,
+    "b": 2
+  }, {
+    "a": 1
+  }, {
+    "a": 1,
+    "b": 2,
+    "c": 2
+  }], {
+    "a": 1,
+    "b": 2
+  }); // should return [{ "a": 1, "b": 2 }, { "a": 1, "b": 2, "c": 2 }].
+  whatIsInAName([{
+    "a": 1,
+    "b": 2
+  }, {
+    "a": 1
+  }, {
+    "a": 1,
+    "b": 2,
+    "c": 2
+  }], {
+    "a": 1,
+    "c": 2
+  }); // should return [{ "a": 1, "b": 2, "c": 2 }].
